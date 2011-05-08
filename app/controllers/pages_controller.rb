d365b62f7359a0cadcdf8fe0872e0e0011de79f9
Georges Duplessy <gduplessy@gmail.com>
2011-05-08 15:53:12 -0400

    getting token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,9 @@ class PagesController < ApplicationController
 
     session[:access_token] = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
     
-    redirect_to session[:access_token] ? success_path : failure_path
+    #redirect_to session[:access_token] ? success_path : failure_path
+
+    redirect_to '/'
   end
   
 end
