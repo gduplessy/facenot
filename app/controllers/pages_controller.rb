2f272ee69abc77d93d0108d8965043f4b17b40b0
Georges Duplessy <gduplessy@gmail.com>
2011-05-08 14:58:58 -0400

    getting uid back

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
 
   def index
+    graph = Koala::Facebook::GraphAPI.new('194345430580007|ad910226438f313f411b99fe-549563502|yNzMstNxGGnYNAHhyoSRBvCiBGs')
+    @profile = graph.get_object("me")
+    @test = graph.get_object("503174896")
+    @friends = graph.get_connections("me", "friends")
+    @gen
   end
+  
+  def redirect
+    session[:access_token] = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
+    
+    redirect_to session[:access_token] ? success_path : failure_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Facenot::Application.routes.draw do
   resources :pages
 
   root :to => "pages#index"
+
+  match '/redirect' => "pages#redirect"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
