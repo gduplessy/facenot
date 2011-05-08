be6c1ae1ba4a7bea5725af7b13ded69a10b33d95
Georges Duplessy <gduplessy@gmail.com>
2011-05-08 02:17:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facenot::Application.config.secret_token = 'e9603945812995f83ec5330d3b525f2b8ae8fbad3cd99f627b2c28680dcacccadc1e0d7d2e280b2e01865e911ea0fa78cf94e3ed5372e77dabd0d9e40b586690'
