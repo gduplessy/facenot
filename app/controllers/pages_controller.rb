1950a8b64fdffb25f8df6ec381e64dc57f3937e3
Georges Duplessy <gduplessy@gmail.com>
2011-05-08 15:50:45 -0400

    getting token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
   end
   
   def redirect
+    oauth_redirect_url = "http://electric-flower-607.heroku.com/redirect"
     @graph2 = Koala::Facebook::GraphAPI.new('194345430580007|ad910226438f313f411b99fe-549563502|yNzMstNxGGnYNAHhyoSRBvCiBGs')
 
     @friends = graph.get_connections("me", "friends")
