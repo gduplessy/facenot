cbb4b698bcecb9b32675082a7abd996f4abb6d9f
Georges Duplessy <gduplessy@gmail.com>
2011-05-10 20:16:00 -0400

    force create

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,12 +11,9 @@ class PagesController < ApplicationController
   
   def redirect
     oauth_redirect_url = "http://electric-flower-607.heroku.com/redirect"
-
-
-    #@graph2 = Koala::Facebook::GraphAPI.new('2227470867|2.KwWPjfyit2ge4TUwi7nzeA__.3600.1305003600.0-100001377458845|o2KsSN4dZRjvkTp4fGSn1aoC_0A')
     oauth_code = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
-    @graph2 = Koala::Facebook::GraphAPI.new(oauth_code)
 
+    @graph2 = Koala::Facebook::GraphAPI.new(oauth_code)
     @friends = @graph2.get_connections("me", "friends")
 
     
