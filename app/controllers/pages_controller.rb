bca2c2e054c0efd23b404dd0c39dc9762d10c797
Georges Duplessy <gduplessy@gmail.com>
2011-05-11 23:21:46 -0400

    typo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,17 @@ class PagesController < ApplicationController
   end
   
   def redirect
-    Resque.enqueue(Gender, params[:code])
+    #Resque.enqueue(Gender, params[:code])
+    oauth_redirect_url = "http://electric-flower-607.heroku.com/redirect"
+    oauth_code = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
+
+    @graph2 = Koala::Facebook::GraphAPI.new(oauth_code)
+    @friends = @graph2.get_connections("me", "friends")
+
+    
+    #redirect_to session[:access_token] ? success_path : failure_path
+
+    redirect_to '/'
   end
   
 end
