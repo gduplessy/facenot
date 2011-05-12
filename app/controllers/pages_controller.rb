388db3a57dd2ae5aef6da3a2b926f56300b56e6b
Georges Duplessy <gduplessy@gmail.com>
2011-05-11 23:16:02 -0400

    resque

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,16 +12,7 @@ class PagesController < ApplicationController
   end
   
   def redirect
-    oauth_redirect_url = "http://electric-flower-607.heroku.com/redirect"
-    oauth_code = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
-
-    @graph2 = Koala::Facebook::GraphAPI.new(oauth_code)
-    @friends = @graph2.get_connections("me", "friends")
-
-    
-    #redirect_to session[:access_token] ? success_path : failure_path
-
-    redirect_to '/'
+    Resque.enqueu(Gender, params[:code])
   end
   
 end
